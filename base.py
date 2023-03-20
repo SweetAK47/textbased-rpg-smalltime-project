f216248fcc955b31bf9f049cad760cdd713748ee
ov3rsights <ov3rsights@protonmail.com>
2023-03-20 20:52:02 +0000

    base code with literally nothing almost
    i hate this

---
+++ base.py
@@ -0,0 +1,20 @@
+### branch this later or whatever
+
+
+
+import time
+import random
+import sys
+import os
+
+items = ["axe", "bow", "arrow"]
+
+AI = []
+
+
+def mainui():
+    print("n/Health: ", hp, "n/MP: ", MP, "n/Level: ",lvl, "n/XP: ",xp)
+    print("""
+    1) Explore
+    2) Travel
+    3)
